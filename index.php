7e36e8031719a61b0d4afd09f2140f44a9c6d9d6
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-01-05 16:52:54 -0500

    added submit button

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <label><input type="radio" value="red" name="color">Red</label><br>
     <label><input type="radio" value="blue" name="color">Blue</label><br>
     <label><input type="radio" value="purple" name="color">Purple</label><br>
-
+    <input type="submit" value="Submit">
 </form>
 </body>
 </html>
