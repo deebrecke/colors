4029ca2c9becb21adfe17c3b40aa49593dfc1d4d
Dee Brecke <brecke.deirdre@student.greenriver.edu>
2023-01-05 16:46:27 -0500

    Added radio buttons

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pick a Color</title>
+
 </head>
 <body>
 <h1>Pick a Color</h1>
+<form method="post" action="confirm.php">
+    <label><input type="radio" value="red" name="color">Red</label><br>
+    <label><input type="radio" value="blue" name="color">Blue</label><br>
+    <label><input type="radio" value="purple" name="color">Purple</label><br>
+
+</form>
 </body>
 </html>
